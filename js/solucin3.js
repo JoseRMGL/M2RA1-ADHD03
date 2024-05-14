2efbe0dcc9121cff4e7343e8c41b5a994933c3d6
JoseRMGL <joseandresr183@gmail.com>
2024-05-14 11:24:36 -0600

    archivo jquery subido

---
+++ js/solucin3.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    const numero1 = $("#numero1").val();
+    const numero2 = $("#numero2").val();
+    const texto1 = $("#texot1").val();
+    const texto2 = $("#texto2").val();
+
+    $("#")
+});
